5e2bf08d2ace3a6dc1a905c304a9123130308aaf
adampriley1 <adampriley1@hotmail.com>
2023-11-21 14:38:45 +0000

    fixed code to show password generated on webpage

--- starter/script.js
+++ starter/script.js
@@ -87,76 +87,89 @@ var upperCasedCharacters = [
   "Y",
   "Z",
 ];
-var passwordLength = 0
+var passwordLength = 0;
 var selectedOption = [];
 
-function getPasswordOptions() {
-  
+function generatePassword() {
   //gets the length of password from user (If < 8, then show warning prompt, if greater than 127, show warning prompt.  Store input as passwordLength)
 
-  passwordLength = prompt("How many characters long do you want your password to be?");
-
+  passwordLength = prompt(
+    "How many characters long do you want your password to be?"
+  );
 
   if (isNaN(passwordLength)) {
-  alert("Please enter numbers only");
-  passwordLength = prompt("How many characters long do you want your password to be?");
-
-} else if (passwordLength < 8) {
-    alert("Minimum password length is 8 characters")
-    passwordLength = prompt("How many characters long do you want your password to be?");
-  
-
+    alert("Please enter numbers only");
+    passwordLength = prompt(
+      "How many characters long do you want your password to be?"
+    );
+  } else if (passwordLength < 8) {
+    alert("Minimum password length is 8 characters");
+    passwordLength = prompt(
+      "How many characters long do you want your password to be?"
+    );
   } else if (passwordLength > 128) {
     alert("Maximum password length is 128 characters");
-    passwordLength = prompt("How many characters long do you want your password to be?");
-
-  }else {
+    passwordLength = prompt(
+      "How many characters long do you want your password to be?"
+    );
+  } else {
     validatedPasswordLength = parseInt(passwordLength);
   }
 
   //Does the user want to include uppercase? if so, add to array "selectedOption"
 
-  
-  let upperCaseYes = confirm ("Click ok to confirm to include uppercase Characters");
+  let upperCaseYes = confirm(
+    "Click ok to confirm to include uppercase Characters"
+  );
   if (upperCaseYes === true) {
-  selectedOption = upperCasedCharacters.concat(selectedOption);
+    selectedOption = upperCasedCharacters.concat(selectedOption);
   }
 
   //Does the user want to include lowercase? if so, add to array "selectedOption"
-  let lowerCaseYes = confirm ("Click ok to confirm to include lowercase Characters");
+  let lowerCaseYes = confirm(
+    "Click ok to confirm to include lowercase Characters"
+  );
   if (lowerCaseYes === true) {
     selectedOption = lowerCasedCharacters.concat(selectedOption);
-  
   }
 
   //Does the user want to include numbers? if so, add to array "selectedOption"
-  let numbersYes = confirm ("Click ok to confirm to include numbers");
+  let numbersYes = confirm("Click ok to confirm to include numbers");
   if (numbersYes === true) {
     selectedOption = numericCharacters.concat(selectedOption);
-   
   }
-  
+
   //Does the user want to include symbols? if so, add to array "selectedOption"
-  let symbolsYes = confirm ("Click ok to confirm to include symbols");
+  let symbolsYes = confirm("Click ok to confirm to include symbols");
   if (symbolsYes === true) {
     selectedOption = specialCharacters.concat(selectedOption);
-   
   }
- 
-//If the user does not select any options, alert and exit
-  if (upperCaseYes === false && lowerCaseYes === false && numbersYes === false && symbolsYes === false){
-  alert("At least 1 character type must be selected");
-}
-else {
-  getRandom()
-}
+
+  //If the user does not select any options, alert and exit
+  if (
+    upperCaseYes === false &&
+    lowerCaseYes === false &&
+    numbersYes === false &&
+    symbolsYes === false
+  ) {
+    alert("At least 1 character type must be selected");
+  } else {
+    return getRandom();
+  }
 }
 
+// Function for getting a random element from an array
 
+var randomPassword = "";
+var generatedPassword = "";
 
-// Function to generate password with user input
-function generatePassword() {
-  getPasswordOptions();
+function getRandom() {
+  for (var i = 0; i < passwordLength; i++) {
+    generatedPassword +=
+      selectedOption[Math.floor(Math.random() * selectedOption.length)];
+  }
+
+  return generatedPassword;
 }
 
 // Get references to the #generate element
@@ -166,21 +179,8 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// //Generate a password when the button is clicked
-// * Present a series of prompts for password criteria
-// * Length of password
-//   * At least 8 characters but no more than 128.
-// * Character types
-//   * Lowercase
-//   * Uppercase
-//   * Numeric
-//   * Special characters ($@%&*, etc)
-// * Code should validate for each input and at least one character type should be selected
-// * Once prompts are answered then the password should be generated and displayed in an alert or written to the page
